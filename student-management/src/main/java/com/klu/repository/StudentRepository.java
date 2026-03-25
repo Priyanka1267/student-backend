4334eeb0c551b1267e3254144914cb1f952be691
Priyanka Sanikommu <sanikommu2024@gmail.com>
2026-03-25 15:14:28 +0530

    Delete student-management directory

--- student-management/src/main/java/com/klu/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klu.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
